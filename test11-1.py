9f19b8a907817e51e6e63d61fb47e03c9d3751ac
Kazuki Asano <cybaron@gmail.com>
2011-06-18 16:04:01 +0900

    add samile files

---
+++ test11-1.py
@@ -0,0 +1,7 @@
+#coding:utf-8
+
+amari = 10 % 3
+
+if amari != 0:
+  print u"割りきれませんでした"
+  print u"余りは", str(amari), u"です"
---
+++ test11-2.py
@@ -0,0 +1,16 @@
+#coding:utf-8
+
+print "Start"
+x = 0
+y = 1
+
+if x == 0:
+  print "x is 0"
+
+  if y == 0:
+    print "y is 0"
+  
+  if y != 0:
+    print "y is not 0"
+
+print "End"
